24229cac784d48817fb6c1bfd3629bc3b74d24fb
rainny <nattaya@sirin.ac.th>
2018-06-27 16:22:01 +0700

    finish 27June

---
+++ app/src/main/java/com/example/kong/rainny/fragment/MainFragment.java
@@ -0,0 +1,22 @@
+package com.example.kong.rainny.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kong.rainny.R;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   //Main Class
