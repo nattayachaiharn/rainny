0b00e527f4481ef09b956c9e5619d917c26e2e94
rainny <nattaya@sirin.ac.th>
2018-06-28 12:12:35 +0700

    Before Lunch

--- app/src/main/java/com/example/kong/rainny/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/kong/rainny/fragment/RegisterFragment.java
@@ -1,30 +1,22 @@
 package com.example.kong.rainny.fragment;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kong.rainny.R;
 
-public class MainFragment extends Fragment{
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-//      Register controller
-
-    }   // Main Method
+public class RegisterFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
     }
-}   //Main Class
+}
